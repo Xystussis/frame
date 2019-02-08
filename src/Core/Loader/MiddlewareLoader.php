3eeffab53f64e53dcc92ad882c17ed78440f68dd
Xystussis <pierrevh@gmail.com>
2019-02-08 18:16:02 +0200

    Added local middleware includes as well as framework middleware

--- src/Core/Loader/MiddlewareLoader.php
+++ src/Core/Loader/MiddlewareLoader.php
@@ -3,6 +3,9 @@
 namespace Frame\Core\Loader;
 
 use Frame\Contracts\Loader;
+use Frame\Contracts\Middleware;
+use Frame\Middleware\Access;
+use Frame\Middleware\Sanitizer;
 
 class MiddlewareLoader implements Loader
 {
@@ -11,18 +14,21 @@ class MiddlewareLoader implements Loader
         return preg_replace('/(\.php){1}$/', '', $item);
     }
 
-    public function load(): void
+    private function loadBaseMiddleware(): void
     {
         $firstInstance = null;
         $middleware = array_map([$this, 'stripEnd'],
             array_filter(scandir(BASE_DIR . '/src/Middleware', 1), function ($item) {
                 return !empty(preg_match('/(\.php){1}$/', $item));
-            }));
+            }) ?? []);
 
         $mCount = count($middleware);
-
+        $currentInstance = null;
         if ($mCount > 0) {
             $className = "\\Frame\\Middleware\\" . $middleware[0];
+            /**
+             * @var Middleware $firstInstance
+             */
             $firstInstance = new $className();
             $currentInstance = $firstInstance;
 
@@ -33,6 +39,19 @@ class MiddlewareLoader implements Loader
         }
         if ($firstInstance !== null) {
             $firstInstance->handle();
+        }
+    }
+
+    public function load(): void
+    {
+        $primary = new Access();
+        $primary->setSuccessor(new Sanitizer());
+
+        $primary->handle();
+
+        //Load any locally produced middleware
+        if (is_dir(BASE_DIR.'/Middleware')) {
+            $this->loadBaseMiddleware();
         }
     }
 }
